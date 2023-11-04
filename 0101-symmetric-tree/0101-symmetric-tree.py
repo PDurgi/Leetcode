76e6e9f7c0f8f8cc5e99dd1dd28980c724344327
Pooja Durgi <97819573+PDurgi@users.noreply.github.com>
2023-11-04 16:46:45 -0400

    Time: 43 ms (45.28%), Space: 16.3 MB (81.56%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.py
+++ 0101-symmetric-tree/0101-symmetric-tree.py
@@ -12,4 +12,4 @@ class Solution:
             return True
         if t1 is None or t2 is None:
             return False
-        return ( t1.val ==t2.val ) and self.mirror(t1.right, t2.left) and self.mirror(t1.left, t2.right )        
+        return t1.val ==t2.val and self.mirror(t1.right, t2.left) and self.mirror(t1.left, t2.right )        
