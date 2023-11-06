ff8ccb065c2372c7c49de084ebde09af205d01c2
Pooja Durgi <97819573+PDurgi@users.noreply.github.com>
2023-11-06 18:18:24 -0500

    Time: 46 ms (21.83%), Space: 16.5 MB (63.04%) - LeetHub

--- 1029-vertical-order-traversal-of-a-binary-tree/1029-vertical-order-traversal-of-a-binary-tree.py
+++ 1029-vertical-order-traversal-of-a-binary-tree/1029-vertical-order-traversal-of-a-binary-tree.py
@@ -14,7 +14,6 @@ class Solution:
         queue= collections.deque([(0,root)])
         x_min=float('inf')
         x_max=float('-inf')
-        y_cord=0
         y_min=0
         y_max=0
         if not root:
@@ -25,15 +24,13 @@ class Solution:
                 x_cord,node=queue.popleft()
                 if y_max not in level_nodes[x_cord]:
                     level_nodes[x_cord][y_max] = []
-                level_nodes[x_cord][y_cord].append(node.val)
+                level_nodes[x_cord][y_max].append(node.val)
                 x_min=min(x_min,x_cord)
                 x_max=max(x_max,x_cord)
                 if node.left:
                     queue.append((x_cord - 1, node.left))
                 if node.right:
                     queue.append((x_cord + 1, node.right))
-
-            y_cord+=1
             y_max+=1
         for i in range(x_min, x_max + 1):
             if i in level_nodes:
