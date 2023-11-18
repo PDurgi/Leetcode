9d50cbd908122ea9583e9e9057ac6d3bb9f36346
Pooja Durgi <97819573+PDurgi@users.noreply.github.com>
2023-11-18 14:06:23 -0500

    Time: 95 ms (77.63%) | Memory: 17.6 MB (88.82%) - LeetSync

--- 207-course-schedule/course-schedule.py
+++ 207-course-schedule/course-schedule.py
@@ -3,13 +3,14 @@ class Solution:
         #pre-req : Topological sort
         #if topological sort is possible ( there is no cycle) , we say that we can finish all courses
         # if topo sort is not possible then len(topo array)  will not be equal to number of nodes
+        # imp: indicates that you must take course bi first if you want to take course ai.
         indegree=[0]*numCourses
         queue=collections.deque()
         adj_list=collections.defaultdict(list)
         
         for prereq in prerequisites:
-            adj_list[prereq[0]].append(prereq[1])
-            indegree[prereq[1]]+=1
+            adj_list[prereq[1]].append(prereq[0])
+            indegree[prereq[0]]+=1
         for i in range(numCourses):
             if indegree[i]==0:
                 queue.append(i)
@@ -24,7 +25,6 @@ class Solution:
                     #push into queue only if indegree==0
                     queue.append(neighbour)
         
-        if len(topo_sort)==numCourses:
-            return True
-        return False
+        return len(topo_sort) == numCourses
+
         
