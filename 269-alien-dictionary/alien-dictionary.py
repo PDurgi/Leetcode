ad48a899f0e7f32a523955d8288be2f087e3ce7d
Pooja Durgi <97819573+PDurgi@users.noreply.github.com>
2023-11-21 23:37:43 -0500

    Time: 45 ms (35.19%) | Memory: 16.3 MB (75.42%) - LeetSync

--- 269-alien-dictionary/alien-dictionary.py
+++ 269-alien-dictionary/alien-dictionary.py
@@ -1,39 +1,32 @@
-from collections import defaultdict
-from queue import Queue
+from typing import List
+import collections
+
 class Solution:
     def alienOrder(self, words: List[str]) -> str:
-        n = len(words)
-        adj = {c:set() for w in words for c in w}
-        q = Queue(maxsize=0)
-        indegree = defaultdict(int)
-        for i in range(n-1):
-            minLen = min(len(words[i]), len(words[i+1]))
-            w1, w2 = words[i], words[i+1]
-            if w1[:minLen] == w2[:minLen] and len(w1) > len(w2): return ""
-            
-            for j in range(minLen):
-                if w1[j] != w2[j]:
-                    if w2[j] not in adj[w1[j]]:
-                        adj[w1[j]].add(w2[j])
-                        indegree[w2[j]] += 1
+        # create adj list from dictionary and in_degree
+        G=collections.defaultdict(set) # Graph
+        in_degree={c:0 for word in words for c in word }
+        for former_word,latter_word in zip(words,words[1:]):
+            for c,d in zip(former_word,latter_word):
+                if c!=d:
+                    if d not in G[c]:
+                        G[c].add(d) # c < d
+                        in_degree[d]+=1
                     break
-        
-               
-        # kahan's algorithm for topological sort
-        for u in adj.keys():
-            if indegree[u] == 0:
-                q.put(u)
-        
-        res = ""
-        while not q.empty():
-            u = q.get()
-            res += u
-            for v in adj[u]:
-                indegree[v] -= 1
-                if indegree[v] == 0:
-                    indegree.pop(v)
-                    q.put(v)
-                    
-        if len(res) != len(adj):
-            return ""
-        return res
+            else:
+                # e.g., 
+                if len(former_word)>len(latter_word):
+                    return ""
+                
+        ans=[]
+        q=collections.deque([c for c in in_degree if in_degree[c]==0])
+        while q:
+            c=q.popleft()
+            ans.append(c)
+            for d in G[c]: # c < d
+                in_degree[d]-=1
+                if in_degree[d]==0:
+                    q.append(d)
+        if len(ans) < len(in_degree):
+            return ""   
+        return ''.join(ans)
