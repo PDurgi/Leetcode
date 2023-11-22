89f071d930ad968e9a9be0eb7719f49b1a642c21
Pooja Durgi <97819573+PDurgi@users.noreply.github.com>
2023-11-21 23:46:48 -0500

    Time: 45 ms (35.19%) | Memory: 16.4 MB (47.92%) - LeetSync

--- 269-alien-dictionary/alien-dictionary.py
+++ 269-alien-dictionary/alien-dictionary.py
@@ -4,26 +4,36 @@ import collections
 class Solution:
     def alienOrder(self, words: List[str]) -> str:
         # create adj list from dictionary and in_degree
-        G=collections.defaultdict(set) # Graph
-        in_degree={c:0 for word in words for c in word }
-        for former_word,latter_word in zip(words,words[1:]):
-            for c,d in zip(former_word,latter_word):
-                if c!=d:
-                    if d not in G[c]:
-                        G[c].add(d) # c < d
-                        in_degree[d]+=1
+        # create adj list from dictionary and in_degree
+        n = len(words)
+        adj = {c:set() for w in words for c in w}
+
+        in_degree = defaultdict(int)
+        for i in range(n-1):
+            minLen = min(len(words[i]), len(words[i+1]))
+            w1, w2 = words[i], words[i+1]
+            if w1[:minLen] == w2[:minLen] and len(w1) > len(w2): return ""
+            
+            for j in range(minLen):
+                if w1[j] != w2[j]:
+                    if w2[j] not in adj[w1[j]]:
+                        adj[w1[j]].add(w2[j])
+                        in_degree[w2[j]] += 1
                     break
-            else:
-                # e.g., 
-                if len(former_word)>len(latter_word):
-                    return ""
+
+        
+        queue=collections.deque()
+        
+        for vertex in adj.keys():
+            if in_degree[vertex]==0:
+                queue.append(vertex)
                 
         ans=[]
         q=collections.deque([c for c in in_degree if in_degree[c]==0])
         while q:
             c=q.popleft()
             ans.append(c)
-            for d in G[c]: # c < d
+            for d in adj[c]: # c < d
                 in_degree[d]-=1
                 if in_degree[d]==0:
                     q.append(d)
