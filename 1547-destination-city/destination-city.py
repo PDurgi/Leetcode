d44ee260658a3a281f5fb257d900114c88a05e4f
PDurgi <97819573+PDurgi@users.noreply.github.com>
2023-12-18 17:58:12 -0500

    Time: 65 ms (36.93%) | Memory: 16.4 MB (19.49%) - LeetSync

--- 1547-destination-city/destination-city.py
+++ 1547-destination-city/destination-city.py
@@ -1,19 +1,30 @@
 import collections
 class Solution:
     def destCity(self, paths: List[List[str]]) -> str:
-        #if we do a bfs if space complexity is not an issue
+        #brute force =>if we do a bfs if space complexity is not an issue
         
-        adj_list=collections.defaultdict(list)
-        queue=collections.deque()
+        # adj_list=collections.defaultdict(list)
+        
+        # for path in paths:
+        #     adj_list[path[0]].append(path[1])
+        # queue=collections.deque()
+        # queue.append(paths[0][0])
+        # destination=None
+        # while queue:
+        #     node=queue.popleft()
+        #     for neighbour in adj_list[node]:
+        #         queue.append(neighbour)
+        # return node
+
+        source_set=set()
         for path in paths:
-            adj_list[path[0]].append(path[1])
-        queue.append(paths[0][0])
-        destination=None
-        while queue:
-            node=queue.popleft()
-            for neighbour in adj_list[node]:
-                queue.append(neighbour)
-        return node
+            source_set.add(path[0])
+        for path in paths:
+            if path[1] not in source_set:
+                return path[1]
+            
+
+
 
 
             
