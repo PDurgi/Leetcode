66433befc56022d754cadf5334b7d89e810228e3
PDurgi <97819573+PDurgi@users.noreply.github.com>
2023-12-22 22:29:34 -0500

    Time: 31 ms (92.99%) | Memory: 17.5 MB (5.49%) - LeetSync

--- 1619-path-crossing/path-crossing.py
+++ 1619-path-crossing/path-crossing.py
@@ -5,10 +5,10 @@ class Solution:
         visited = set()
         (row, col) = (0, 0)
         my_dict = collections.defaultdict(list)
-        my_dict['N'].append((-1, 0))  # Adjusted direction vectors
-        my_dict['E'].append((0, 1))
-        my_dict['W'].append((0, -1))
-        my_dict['S'].append((1, 0))
+        my_dict['N'].append((0,1))
+        my_dict['E'].append((1,0))
+        my_dict['W'].append((-1,0))
+        my_dict['S'].append((0,-1))
 
         visited.add((0, 0))
         for p in path:
