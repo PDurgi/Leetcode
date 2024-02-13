a362d80712d7bcce47ea4746b84142c589013739
PDurgi <97819573+PDurgi@users.noreply.github.com>
2024-02-13 12:04:29 -0500

    Time: 43 ms (22.38%) | Memory: 16.5 MB (70.27%) - LeetSync

---
+++ 62-unique-paths/unique-paths.py
@@ -0,0 +1,26 @@
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+       #let us look at memorization approach-- top-down 
+       # we will express the base cases and recursion in terms of indexes as first step
+       # explore all the possibilites / count all ways
+       # sum up the number of possibilites
+        dp = [[-1 for _ in range(n)] for _ in range(m)]
+        def path_rec(i,j,dp):
+            if i==0 and j==0:
+                dp[i][j] = 1
+                return dp[i][j]
+            # Note when we are counting ways or counting path, we will return a 1 on base case to keep the count.
+            if dp[i][j]!=-1:
+                return dp[i][j] 
+            if i<0 or j<0:
+                return 0 #For a failure that is going out of bounds or out of path, we will return a 0.
+            # since we are going top-down, that is starting from the destination, we will take reverse directions, i.e go up and left
+
+            #when we move up, we reduce the row index by 1
+            #when we move left, we reduce the col index by 1
+
+            up=path_rec(i-1,j,dp)
+            left=path_rec(i,j-1,dp)
+            dp[i][j]=up+left
+            return dp[i][j]
+        return path_rec(m-1,n-1,dp)
